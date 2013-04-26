d43b46b42e1ff392c0515a63dbe3227abc23ed04
Michael Wheeler <mwheeler@g2labs.net>
2013-04-26 16:31:43 -0500

    CORS

--- app.js
+++ app.js
@@ -1,7 +1,16 @@
 var express = require("express");
 var app = express();
 var coords = { lat: 0, lon: 0 };
+var allowCrossDomain = function(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+  res.header('Access-Control-Allow-Headers', 'Content-Type');
+
+  next();
+};
+
 app.use(express.logger());
+app.use(allowCrossDomain);
 
 app.get('/', function (request, response) {
   response.send(JSON.stringify(coords));
