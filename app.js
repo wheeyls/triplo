fc27a1c3966295b053e7d3678fd4bd68b9d0232d
Michael Wheeler <mwheeler@g2labs.net>
2013-04-26 16:24:27 -0500

    do it

--- app.js
+++ app.js
@@ -1,9 +1,18 @@
 var express = require("express");
 var app = express();
+var coords = { lat: 0, lon: 0 };
 app.use(express.logger());
 
-app.get('/', function(request, response) {
-  response.send('Hello World!');
+app.get('/', function (request, response) {
+  response.send(JSON.stringify(coords));
+});
+
+app.post('/', function (request, response) {
+  try {
+    coords = JSON.parse(request.params);
+  } catch (err) {
+    coords = { lat: 0, lon: 0 };
+  }
 });
 
 var port = process.env.PORT || 5000;
